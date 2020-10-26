dd8a6d1acfda8f6c3f5afb647ab0c461e628e65d
Coschchack <mich.kosciak@gmail.com>
2020-10-26 12:57:54 +0100

    reformat

--- ghibli_api/api_client.py
+++ ghibli_api/api_client.py
@@ -11,14 +11,14 @@ class ApiClient:
     def get(endpoint):
         return requests.get(endpoint)
 
-    async def async_get(self, client_session, url_, **kwargs):
+    async def get_concurrently(self, urls):
+        async with ClientSession() as session:
+            coroutines = (self._send_single_concurrent_get(session, url_) for url_ in urls)
+            results = await asyncio.gather(*coroutines)
+        return results
+
+    async def _send_single_concurrent_get(self, client_session, url_, **kwargs):
         # Also limit the number of concurrent requests
         async with self.SEMAPHORE, client_session.get(url_, verify_ssl=False, **kwargs) as resp:
             resp_json = await resp.json()
             return resp_json
-
-    async def async_gets(self, urls):
-        async with ClientSession() as session:
-            coroutines = (self.async_get(session, url_) for url_ in urls)
-            results = await asyncio.gather(*coroutines)
-        return results
--- ghibli_movies/views.py
+++ ghibli_movies/views.py
@@ -12,7 +12,7 @@ from ghibli_api.api_info_extractor import ApiInfoExtractor
 
 @cache_page(60)
 def movies_list_view(request):
-    api_responses = asyncio.run(ApiClient().async_gets((Urls.FILMS_URL.value, Urls.PEOPLE_URL.value)))
+    api_responses = asyncio.run(ApiClient().get_concurrently((Urls.FILMS_URL.value, Urls.PEOPLE_URL.value)))
     maps_collection = ApiInfoExtractor().get_people_to_movie_maps(
         all_films=api_responses[0], all_people=api_responses[1])
     context = {
